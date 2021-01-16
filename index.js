bc117da95e18a4bb3b54000d671ec63b99d906f5
HORRIBLE12 <albert_destroyer@protonmail.com>
2021-01-16 14:53:34 +0000

    done

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var greet = "Hello";
-var name = "Bruce";
+var name = "Bruce!";
 
 // only change code below this line
-
-var str = "";
+var there="there,";
+var str = greet + ' ' + there +' '+name;
 
 // only change code above this line
 
